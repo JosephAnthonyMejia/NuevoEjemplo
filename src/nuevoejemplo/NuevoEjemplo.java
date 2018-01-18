277e46e6354cfbc56dfbaf1ebbade7bfa1ba3751
joseph <>
2018-01-17 21:05:49 -0500

    primer clonada

--- src/nuevoejemplo/NuevoEjemplo.java
+++ src/nuevoejemplo/NuevoEjemplo.java
@@ -16,7 +16,7 @@ public class NuevoEjemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola  nuevo ejuemplo");
+        System.out.println("Hola  nuevo ejemplo");
     }
     
 }
